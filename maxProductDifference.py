9253e223ed12267f66585d934c093a2c4aa28b78
Karim Chemaytilly <kchemaytilly@gmail.com>
2022-05-20 09:45:11 +0200

    v1

--- maxProductDifference.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxProductDifference(self, nums: list[int]) -> int:
-        nums.sort()
-        a = nums[len(nums) - 1]
-        b = nums[len(nums) - 2]
-        c = nums[0]
-        d = nums[1]
-        return (a * b) - (c * d)
